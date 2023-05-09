6546e60a10cd388ef111b145ff503815fb1e867c
Shubham <shubham.219301072@muj.manipal.edu>
2023-05-09 19:46:20 +0530

    add games/tictactoe

---
+++ games/tictactoe/README.md
@@ -0,0 +1,51 @@
+<a name="readme-top"></a>
+
+<div align="center">
+  <h3 align="center">Games, TicTacToe</h3>
+  Play TicTacToe with your friends online!
+  <p align="center">
+    <a href="https://github.com/shubhamistic/shubhamistic.github.io/tree/main/games/tictactoe"><strong>Explore the docs »</strong></a>
+    <br />
+    ·
+    <a href="https://github.com/shubhamistic/shubhamistic.github.io/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/shubhamistic/shubhamistic.github.io/issues">Request Feature</a>
+    ·
+  </p>
+</div>
+
+
+## Visit
+[shubhamistic.com/games/tictactoe](https://shubhamistic.com/games/tictactoe)
+
+
+## Demo
+
+<img src="images/demo.png" alt="N-Queens Demo" width="100%">
+
+
+## About
+
+Introducing the ultimate multiplayer Tic Tac Toe experience!
+Immerse yourself in the stunning visuals of our beautifully
+designed webpage, made possible with the power of Flask and 
+Socket.IO. Play with friends and challenge them to a game of 
+strategy and wits. It's time to show off your Tic Tac Toe 
+skills and dominate the competition!
+
+
+## Tech Stack
+- HTML
+- CSS
+- JavaScript
+- jQuery
+
+
+## Author
+[@shubhamistic](https://www.github.com/shubhamistic)
+
+
+## Feedback
+If you have any feedback, please reach out to me at my email address [shubham2003garg@gmail.com](mailto:shubham2003garg@gmail.com)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ games/tictactoe/script.js
@@ -0,0 +1,532 @@
+(() => {
+    // (global) variables
+    let marker = null; // Variable to store either 'X' or 'O'
+    let room_code = null;
+    let token = null;
+    let participant_type = null;
+    let validTurn = false; // Variable to track whether it is the player's turn or the opponent's turn.
+
+    // define socket
+    const socket = io('example.com', {autoConnect: false});
+
+    // define all the buttons
+    let joinButton = $("#join-button");
+    let createRoomButton = $("#create-room-button");
+    let exitButton = $("#exit-button");
+
+    // define all prompts
+    let prompts = $('#prompts'); // prompts container
+    let prompt1 = $("#prompt-1"); // INSTRUCTIONS prompt
+    let prompt2 = $("#prompt-2"); // YOU WON! || OPPONENT WON! || MATCH DRAW! prompt
+    let prompt3 = $("#prompt-3"); // OPPONENT LEFT! || ROOM EXPIRED! prompt
+
+    // tic-tac-toe board area
+    let tttBoardDiv = $('#ttt-board-div');
+
+    // tic-tac-toe box
+    let tttBox = $('#ttt-board .ttt-box');
+
+    // error-message
+    let errorMsg = $('#input-room-div form #error-message');
+
+    // user input
+    let roomCodeInput = $('#input-room-div form input');
+
+    // create markers html
+    let cross_marker = (`
+        <div class="marker">
+            <i class="fa-solid fa-xmark cross-marker"></i>
+        </div>
+    `)
+    let circle_marker = (`
+        <div class="marker">
+            <div class="circle-marker"></div>
+        </div>
+    `)
+
+    // marker value
+    let markerValue = $('#marker-value');
+
+    // prevent form to reload the page
+    $("#input-room-div form").submit(function(e) {
+        e.preventDefault();
+    });
+
+    // function to bring the webpage to its default state
+    function defaults() {
+        // set all (global) variable to their default values
+        marker = null;
+        room_code = null;
+        token = null;
+        participant_type = null;
+        validTurn = false;
+
+        // disable exit button click
+        exitButton.prop('disabled', true);
+        exitButton.css({
+            "cursor": "not-allowed",
+            "background-color": "#7d4a46"
+        });
+
+        // enable the create-room button
+        createRoomButton.html("CREATE");
+        createRoomButton.prop('disabled', false);
+        createRoomButton.css({
+            "cursor": "pointer",
+            "background-color": "goldenrod",
+            "color": "#303030"
+        });
+
+        // enable the join-room button
+        joinButton.prop('disabled', false);
+        joinButton.css({
+            "cursor": "pointer",
+            "background-color": "limegreen"
+        });
+
+        // Disable the tic-tac-toe board
+        tttBoardDiv.attr('disabled', true);
+        tttBoardDiv.css({
+            'pointer-events': 'none',
+            'opacity': '50%'
+        });
+
+        // enable hover events in tic-tac-toe boxes & clear all html inside tic-tac-toe box
+        tttBox.removeClass("lock-ttt-box lock-all-ttt-box");
+        tttBox.empty();
+
+        // enable the prompts
+        prompts.css("display", "block");
+
+        // prompt user to join or create a room
+        prompt1.css("display", "block");
+        prompt1.html('Join or create a room to play Tic Tac Toe! 😉')
+
+        // clear marker value
+        markerValue.css("visibility", "hidden");
+
+        // clear all socket events and disconnect the socket
+        socket.off();
+        socket.disconnect();
+    }
+    // initially set all things to default
+    defaults();
+
+
+    // handle join button click
+    joinButton.click(function(){
+        // take user input
+        let roomCodeInputVal = roomCodeInput.val();
+
+        // ajax call to send server a "join room" request
+        $.ajax({
+            url: "example.com/tictactoe/join-room",
+            type: "POST",
+            data: JSON.stringify({"room_code": roomCodeInputVal}),
+            success: function(response){
+                // if request is fulfilled by the server,
+                if(response["fulfilled"]) {
+                    // set (global) room_code & token variable
+                    room_code = response.room_code;
+                    token = response.token;
+                    participant_type = "guest";
+
+                    // hide error message if entered room code is correct
+                    errorMsg.css("visibility", "hidden");
+
+                    // on join, enable the exit button
+                    exitButton.prop('disabled', false);
+                    exitButton.css({
+                        "cursor": "pointer",
+                        "background-color": "indianred"
+                    });
+
+                    // on join, disable create-room button click
+                    createRoomButton.prop('disabled', true);
+                    createRoomButton.css({
+                        "cursor": "not-allowed",
+                        "background-color": "darkgoldenrod"
+                    });
+
+                    // on join, disable join button click
+                    joinButton.prop('disabled', true);
+                    joinButton.css({
+                        "cursor": "not-allowed",
+                        "background-color": "darkseagreen"
+                    });
+
+                    // program execution handler
+                    startGame(room_code, token, participant_type).then(() => {
+                        // revert to default settings after the game is finished
+                        // defaults();
+                    });
+                }
+                else{
+                    // display error message if entered room code is unavailable
+                    errorMsg.css("visibility", "visible");
+                }
+            },
+            error: function() {
+                // display error message if entered room code is incorrect
+                errorMsg.css("visibility", "visible");
+            }
+        });
+    });
+    joinButton.hover(
+        function () {$(this).css({"background-color": "lawngreen"});},
+        function () {$(this).css({"background-color": "limegreen"});}
+    );
+
+    // handle create-room button click
+    createRoomButton.click(function(){
+        // ajax call to request server to generate a room code
+        $.ajax({
+            url: "example.com/tictactoe/generate-room-code",
+            type: "GET",
+            success: function (response) {
+                // set (global) room_code & token variable
+                room_code = response.room_code;
+                token = response.token;
+                participant_type = "host";
+
+                // hide error message if entered room code is correct
+                errorMsg.css("visibility", "hidden");
+
+                // empty the input value
+                roomCodeInput.val('');
+
+                // prompt user to share the code
+                prompt1.html(`Share the room code (${response.room_code}) with your friend and let the game begin! 😁`);
+
+                // on create-room, enable the exit button
+                exitButton.prop('disabled', false);
+                exitButton.css({
+                    "cursor": "pointer",
+                    "background-color": "indianred"
+                });
+
+                // display the room code on the create-button itself
+                createRoomButton.html(response.room_code);
+                // on join, disable create-room button click
+                createRoomButton.prop('disabled', true);
+                createRoomButton.css({
+                    "cursor": "not-allowed",
+                    "background-color": "#242424",
+                    "color": "whitesmoke"
+                });
+
+                // on join, disable join button click
+                joinButton.prop('disabled', true);
+                joinButton.css({
+                    "cursor": "not-allowed",
+                    "background-color": "darkseagreen"
+                });
+
+                // program execution handler
+                startGame(room_code, token, participant_type).then(() => {
+                    // revert to default settings after the game is finished
+                    // defaults();
+                });
+            },
+            error: function () {/* exception (not necessary to handle) */}
+        });
+    });
+    createRoomButton.hover(
+        function () {$(this).css({"background-color": "gold"});},
+        function () {$(this).css({"background-color": "goldenrod"});}
+    );
+
+
+    // handle exit button click
+    exitButton.click(function(){
+        // ajax call to send server a "exit room" request
+        $.ajax({
+            url: "example.com/tictactoe/exit-room",
+            type: "POST",
+            data: JSON.stringify({
+                "room_code": room_code,
+                "token": token
+            })
+        });
+
+        // break the socket connection from server side
+        socket.emit('exit-room', {
+            "room_code": room_code,
+            "token": token,
+            "participant_type": participant_type
+        });
+
+        // reset to default web-page
+        defaults();
+    });
+    exitButton.hover(
+        function() {$(this).css("background-color", "tomato");},
+        function() {$(this).css("background-color", "indianred");}
+    );
+
+
+    // handle the tic-tac-toe box clicks
+    tttBox.click(function (){
+        let boxId = $(this).attr('id');
+        boxId = (boxId[4] + boxId[5]); // id: "box-00", "00" are at indexes 4 & 5
+
+        // check whether it is the player's turn or the opponent's turn.
+        if(validTurn) {
+            // place the marker
+            socket.emit('place-marker', {
+                "room_code": room_code,
+                "token": token,
+                "participant_type": participant_type,
+                "box_id": boxId
+            });
+        }
+    });
+
+
+    async function startGame(room_code, token, participant_type){
+        // connect to the socket
+        socket.connect();
+
+        // define socket events
+        socket.on('connect', function() {
+            // connected to the socket
+        });
+
+        // host socket events
+        if(participant_type === "host") {
+            // host join handler
+            socket.on('host-joined', function (data) {
+                // host joined, set marker for the host
+                marker = data["marker"];
+
+                // player with marker 'X' will always have first mover advantage
+                if(marker === 'X'){
+                    validTurn = true;
+                    // display marker value
+                    markerValue.html("MARKER: <b>X</b>");
+                    markerValue.css({
+                        "visibility": "visible",
+                        "color": "#f71638"
+                    });
+                }
+                if(marker === 'O'){
+                    // display marker value
+                    markerValue.html("MARKER: <b>O</b>");
+                    markerValue.css({
+                        "visibility": "visible",
+                        "color": "dodgerblue"
+                    });
+
+                    // lock all boxes
+                    tttBox.addClass("lock-all-ttt-box");
+                }
+            });
+
+            // guest join from host point of view handler
+            socket.on('guest-joined', function (data) {
+                // guest joined
+
+                // prompt opponent join successful message
+                prompt1.html("Your opponent 😈 has joined the game!");
+                prompt1.fadeIn(function() {
+                    $(this).delay(1000).fadeOut(function() {
+                        // Enable the tic-tac-toe board
+                        prompts.css("display", "none");
+                        tttBoardDiv.attr('disabled', false);
+                        tttBoardDiv.css({
+                            'pointer-events': 'auto',
+                            'opacity': '100%'
+                        });
+                    });
+                });
+            });
+
+            // host won on host side
+            socket.on('host-won', function (data) {
+                // display you won prompt and reset the game to defaults
+                prompts.css("display", "block");
+                prompt2.html("YOU WON! 🥳");
+                prompt2.fadeIn(function() {
+                    $(this).delay(2000).fadeOut(function() {
+                        prompts.css("display", "none");
+                        defaults();
+                    });
+                });
+            });
+
+            // guest won on host side
+            socket.on('guest-won', function (data) {
+                // display opponent won prompt and reset the game to defaults
+                prompts.css("display", "block");
+                prompt2.html("OPPONENT WON! 😓");
+                prompt2.fadeIn(function() {
+                    $(this).delay(2000).fadeOut(function() {
+                        prompts.css("display", "none");
+                        defaults();
+                    });
+                });
+            });
+
+            // if guest left the game
+            socket.on('guest-left', function (data) {
+                // display opponent left prompt and reset the game to defaults
+                prompts.css("display", "block");
+                prompt3.html("OPPONENT LEFT! 🫠");
+                prompt3.fadeIn(function() {
+                    $(this).delay(1000).fadeOut(function() {
+                        prompts.css("display", "none");
+                        defaults();
+                    });
+                });
+            });
+        }
+
+        // guest socket events
+        if(participant_type === "guest") {
+            // guest join handler
+            socket.on('guest-joined', function (data) {
+                // guest joined, set marker for the guest
+                marker = data["marker"];
+
+                // player with marker 'X' will always have first mover advantage
+                if(marker === 'X'){
+                    validTurn = true;
+                    // display marker value
+                    markerValue.html("MARKER: <b>X</b>");
+                    markerValue.css({
+                        "visibility": "visible",
+                        "color": "#f71638"
+                    });
+                }
+                if(marker === 'O'){
+                    // display marker value
+                    markerValue.html("MARKER: <b>O</b>");
+                    markerValue.css({
+                        "visibility": "visible",
+                        "color": "dodgerblue"
+                    });
+
+                    // lock all boxes
+                    tttBox.addClass("lock-all-ttt-box");
+                }
+
+                // prompt join successful message
+                prompt1.html("You have successfully joined the game! 😉️");
+                prompt1.fadeIn(function() {
+                    $(this).delay(1000).fadeOut(function() {
+                        // Enable the tic-tac-toe board
+                        prompts.css("display", "none");
+                        tttBoardDiv.attr('disabled', false);
+                        tttBoardDiv.css({
+                            'pointer-events': 'auto',
+                            'opacity': '100%'
+                        });
+                    });
+                });
+            });
+
+            // guest won on guest side
+            socket.on('guest-won', function (data) {
+                // display you won prompt and reset the game to defaults
+                prompts.css("display", "block");
+                prompt2.html("YOU WON! 🥳");
+                prompt2.fadeIn(function() {
+                    $(this).delay(2000).fadeOut(function() {
+                        prompts.css("display", "none");
+                        defaults();
+                    });
+                })
+            });
+
+            // host won on guest side
+            socket.on('host-won', function (data) {
+                // display opponent won prompt and reset the game to defaults
+                prompts.css("display", "block");
+                prompt2.html("OPPONENT WON! 😓");
+                prompt2.fadeIn(function() {
+                    $(this).delay(2000).fadeOut(function() {
+                        prompts.css("display", "none");
+                        defaults();
+                    });
+                });
+            });
+
+            // if host left the game
+            socket.on('host-left', function (data) {
+                // display opponent left prompt and reset the game to defaults
+                prompts.css("display", "block");
+                prompt3.html("OPPONENT LEFT! 🫠");
+                prompt3.fadeIn(function() {
+                    $(this).delay(1000).fadeOut(function() {
+                        prompts.css("display", "none");
+                        defaults();
+                    });
+                });
+            });
+        }
+
+        // common host & guest events,
+        // if room code is expired
+        socket.on('room-expired', function (data){
+            // reset to default game page
+            prompts.css("display", "block");
+            prompt3.html("ROOM EXPIRED! 🤕");
+            prompt3.fadeIn(function() {
+                $(this).delay(2000).fadeOut(function() {
+                    prompts.css("display", "none");
+                    defaults();
+                });
+            });
+        });
+
+        // set marker handler
+        socket.on('marker-placed', function (data) {
+            let markedBox = $(`#box-${data["box_id"]}`);
+
+            // lock the box if marker is placed
+            markedBox.addClass("lock-ttt-box");
+
+            // place the marker html
+            if(data["marker"] === 'X'){
+                markedBox.append(cross_marker);
+            }
+            if(data["marker"] === 'O'){
+                markedBox.append(circle_marker);
+            }
+
+            // disable the player board if he has already played his turn
+            if(data["marker"] === marker){
+                validTurn = false;
+                tttBox.each(function() {
+                    if(!$(this).hasClass('lock-ttt-box')){
+                        $(this).addClass("lock-all-ttt-box");
+                    }
+                });
+            }
+            else{
+                validTurn = true;
+                tttBox.removeClass("lock-all-ttt-box");
+            }
+        });
+
+        // match draw
+        socket.on('match-draw', function (data) {
+            // display match draw prompt and reset the game to defaults
+            prompts.css("display", "block");
+            prompt2.html("MATCH DRAW! 😬");
+            prompt2.fadeIn(function() {
+                $(this).delay(1000).fadeOut(function() {
+                    prompts.css("display", "none");
+                    defaults();
+                });
+            });
+        });
+
+        // join the room
+        socket.emit('join-room', {
+            "room_code": room_code,
+            "token": token,
+            "participant_type": participant_type
+        });
+    }
+})();
