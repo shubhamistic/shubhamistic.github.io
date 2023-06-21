dbd7273f751690d27a1262e9b30e7c8e89355d65
Shubham <shubham.219301072@muj.manipal.edu>
2023-06-21 19:13:06 +0530

    update projects section in shubhamistic.com

--- shubhamistic/js/projects.js
+++ shubhamistic/js/projects.js
@@ -1,3 +1,14 @@
 $(()=> {
+    // logic for making the project list scrollable in x-axis
+    const projectList = $('.p-list');
+    projectList.on('wheel swipeleft swiperight', function(event) {
+        let deltaX = event.originalEvent.deltaX || 0;
+        let deltaY = event.originalEvent.deltaY || 0;
 
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            // Scroll the div horizontally
+            $(this).scrollLeft($(this).scrollLeft() - deltaX);
+            event.preventDefault(); // Prevent default scrolling behavior
+        }
+    });
 });
